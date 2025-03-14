f9e245a16febbc56d82f66fecbbfdfc5327696fb
Brandon Gonzalez <bgbeaconhous@gmail.com>
2025-03-14 13:12:08 -0700

    final changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
 import { useState } from 'react'
 import './App.css'
+import { puppyList } from './data'
+import './Style.css'
 
 function App() {
-  
-  
-
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  // console.log("puppyList: ", puppyList);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  // console.log(featuredPup)
+  // console.log(featuredPup)
   return (
     <>
-      <div>
-      
+      <div className="mainDiv">
+        
+      {featPupId && (
+        <div className="moreInfo">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+        {
+         
+          puppies.map((puppy) => {
+            return <p className="eachPup" onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+          })
+        }
+         
+       
+         
+
       </div>
       
       
